dbaefbceba00b12e4893a92dbf93017d5adfd4b0
wokovn <khoa199939@gmail.com>
2026-05-26 23:18:33 +0700

    limit pool size for pods

--- social-backend/modules/db/db.js
+++ social-backend/modules/db/db.js
@@ -4,7 +4,7 @@ import postgres from 'postgres'
 const connectionString = process.env.DATABASE_URL
 
 // Disable prefetch as it is not supported for "Transaction" pool mode
-const client = postgres(connectionString, { prepare: false })
+const client = postgres(connectionString, { prepare: false, max: 5 })
 const db = drizzle(client);
 
 export default db;
